6c22fbb7503aae3f1603ea1d26c7a5fdbf352cd4
felpasl <felipe.lambert@outlook.com>
2019-07-04 01:41:51 -0300

    Add my-trades on backetster_generation

--- commands/sim.js
+++ commands/sim.js
@@ -8,6 +8,7 @@ var tb = require('timebucket')
   , objectifySelector = require('../lib/objectify-selector')
   , engineFactory = require('../lib/engine')
   , collectionService = require('../lib/services/collection-service')
+  , objectsToCsv = require('objects-to-csv')
   , _ = require('lodash')
 
 module.exports = function (program, conf) {
@@ -191,6 +192,10 @@ module.exports = function (program, conf) {
         if (so.backtester_generation >= 0)
         {
           fs.writeFileSync(path.resolve(__dirname, '..', 'simulations','sim_'+so.strategy.replace('_','')+'_'+ so.selector.normalized.replace('_','').toLowerCase()+'_'+so.backtester_generation+'.json'),options_json, {encoding: 'utf8'})
+          var trades_json = JSON.stringify(s.my_trades, null, 2)
+          var trades_name = 'simtrades_'+so.strategy.replace('_','')+'_'+ so.selector.normalized.replace('_','').toLowerCase()+'_'+so.backtester_generation
+          fs.writeFileSync(path.resolve(__dirname, '..', 'simulations',trades_name+'.json'),trades_json, {encoding: 'utf8'})
+          new objectsToCsv(s.my_trades).toDisk(path.resolve(__dirname, '..', 'simulations',trades_name+'.csv'))
         }
 
         if (so.filename !== 'none') {
